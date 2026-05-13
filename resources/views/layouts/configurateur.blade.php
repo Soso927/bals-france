b14012218aa703f5af4df682a258d059841bf577
BET <bet@dom.balsfrance.fr>
2026-05-13 11:33:04 +0200

    fix: suppression du double chargement Alpine.js dans le layout configurateur

--- resources/views/layouts/configurateur.blade.php
+++ resources/views/layouts/configurateur.blade.php
@@ -11,7 +11,6 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     @yield('styles')
 
